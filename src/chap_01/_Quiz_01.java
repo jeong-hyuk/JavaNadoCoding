8bcd9a18a05bc0699ca864cedae9ed68faa25056
jeong-hyuk <jack1150@naver.com>
2023-01-10 03:20:16 +0900

    Feat: "추가 나도코딩 Java 형 변환 + quiz#1"

--- src/chap_01/_Quiz_01.java
+++ src/chap_01/_Quiz_01.java
@@ -2,6 +2,7 @@ package chap_01;
 
 public class _Quiz_01 {
     public static void main(String[] args) {
+        //String, int, double의 형태로 바꿔준다.
         String busNumber = "08";
         int remainTime = 3;
         double remainDistance = 1.2;
