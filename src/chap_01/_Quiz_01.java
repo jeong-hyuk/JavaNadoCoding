994fb675a5aa9ea928919acdaaa48b0acaaef09a
jeong-hyuk <jack1150@naver.com>
2023-01-10 03:02:18 +0900

    Feat: "추가 나도코딩 Java 형 변환 + quiz#1"

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,11 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNumber = "08";
+        int remainTime = 3;
+        double remainDistance = 1.2;
+
+        System.out.println("상암"+busNumber+"번 버스\n약 "+remainTime+"분 후 도착\n남은 거리 "+remainDistance+"km");
+    }
+}
