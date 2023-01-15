016603df3fb20b71c2bc590aa14c821b46bb0645
jeong-hyuk <jack1150@naver.com>
2023-01-15 23:12:42 +0900

    Feat: "추가 나도코딩 Java multiArrayLoop 시작"

---
+++ src/chap_05/_04_MultiArrayLoop.java
@@ -0,0 +1,12 @@
+package chap_05;
+
+public class _04_MultiArrayLoop {
+    public static void main(String[] args) {
+        //다차원 배열 순회
+        String[][] seats = new String[][] {
+                {"A1", "A2", "A3", "A4", "A5"},
+                {"B1", "B2", "B3", "B4", "B5"},
+                {"C1", "C2", "C3", "C4", "C5"}
+        };
+    }
+}
